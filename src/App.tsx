9a1e133d32bfbde16375fd84fb9ddfb4a0017bd2
ManupaDev <manupadevsuite@gmail.com>
2024-01-01 20:59:19 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="p-4">
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
